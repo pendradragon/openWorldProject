46348b199da581c6749515dfe978b81d1ca25faf
Megan <143452991+pendradragon@users.noreply.github.com>
2025-04-27 18:14:23 -0400

    Classes Upload

--- characters/woodChopper/SkeletalMeshComponent.h
+++ characters/woodChopper/SkeletalMeshComponent.h
@@ -1,19 +1,23 @@
 //Copyright Epic Games, Inc. All Rights Resevered
 
 #pragma once
+
 #include "CoreMinimal.h"
 #include "UObject/ObjectMacros.h"
 #include "Engine/EngineTypes.h"
 #include "Engine/EngineBaseTypes.h"
+#include "Components/SceneComponent.h"
 #include "EngineDefines.h"
 #include "CollisionQueryParams.h"
 #include "Interfaces/Interface_CollisionDataProvider.h"
 #include "Animation/AnimationAsset.h"
+#include "Animation/AnimCurveTypes.h"
 #include "Components/SkinnedMeshComponent.h"
 #include "ClothSimData.h"
 #include "SingleAnimationPlayData.h"
 #include "Animation/PoseSnapshot.h"
 #include "ClothingSystemRuntimeTypes.h"
+#include "ClothingSimulationInterface.h"
 #include "ClothingSimulationFactory.h"
 #include "Animation/AttributesRuntime.h"
 #if WITH_ENGINE
@@ -21,3 +25,24 @@
   #endif
 
 #include "SkeletalMeshComponent.generated.h"
+
+
+class Error;
+class FPrimitiveDrawInterface;
+class FCanvas;
+class FSceneView; 
+class UAnimInstance;
+class UPhysicalMaterial; 
+class USkeletalMesh;
+class USkeletalMeshComponent;
+struct FClothCollionSource;
+struct FConstraintInstance;
+struct FConstraintProfileProperties;
+struct FNavigableGometryExport; 
+struct FCompactPose;
+
+enum class EClothingTeleportMode : unit8; 
+
+#ifndef WITH_CLOTH_COLLION DETECTION
+#define WITH_CLOTH_COLLISION_DETECTION 1
+#endif
