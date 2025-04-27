37d47332b1406f20e728632796f4a78fbaa55a2c
Megan <143452991+pendradragon@users.noreply.github.com>
2025-04-27 17:17:57 -0400

    Create SkeletalMeshComponent.h
    going to be working slowly on this while I work on other assignments for other classes.

---
+++ characters/woodChopper/SkeletalMeshComponent.h
@@ -0,0 +1,23 @@
+//Copyright Epic Games, Inc. All Rights Resevered
+
+#pragma once
+#include "CoreMinimal.h"
+#include "UObject/ObjectMacros.h"
+#include "Engine/EngineTypes.h"
+#include "Engine/EngineBaseTypes.h"
+#include "EngineDefines.h"
+#include "CollisionQueryParams.h"
+#include "Interfaces/Interface_CollisionDataProvider.h"
+#include "Animation/AnimationAsset.h"
+#include "Components/SkinnedMeshComponent.h"
+#include "ClothSimData.h"
+#include "SingleAnimationPlayData.h"
+#include "Animation/PoseSnapshot.h"
+#include "ClothingSystemRuntimeTypes.h"
+#include "ClothingSimulationFactory.h"
+#include "Animation/AttributesRuntime.h"
+#if WITH_ENGINE
+  #include "Engine/PoseWatchRenderData.h"
+  #endif
+
+#include "SkeletalMeshComponent.generated.h"
